eaffe2bc50065229cf6de8702422eba8fb444a70
Pappyjedu <148871618+Pappyjedu@users.noreply.github.com>
2024-05-18 23:41:03 +0000

    Create userInfo.js

---
+++ userInfo.js
@@ -0,0 +1,11 @@
+let id = 1;
+
+function createUserProfiles(names, modifiedNames) {
+  return names.map((name, i) => {
+    return {
+      originalName: name,
+      modifiedName: modifiedNames[i],
+      id: id++
+    };
+  });
+}
