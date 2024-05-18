da3dacbce59f846d507208036957d86de14bdb98
Pappyjedu <148871618+Pappyjedu@users.noreply.github.com>
2024-05-18 23:35:09 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,12 @@
 function processArray(arr) {
   return arr.map(num => (num % 2 === 0) ? num * num : num * 3);
 }
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, i) => {
+    if (numbers[i] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
